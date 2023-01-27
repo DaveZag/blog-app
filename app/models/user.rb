58518108ff248ef855fee734b1f37df9ed6c04df
Dave ZAGABE <80624123+DaveZag@users.noreply.github.com>
2023-01-27 12:04:54 +0200

    Add models for all classes as shown in the ERD
    Create sample users, comments, posts

---
+++ app/models/user.rb
@@ -0,0 +1,17 @@
+class User < ApplicationRecord
+  has_many :posts 
+  has_many :likes 
+  has_many :comments
+  after_initialize :update_posts_counter
+
+  def update_posts_counter
+    self.posts_counter = 0 unless posts_counter
+  end
+
+  private :update_posts_counter
+
+  # return 3 latests posts
+  def latest_posts
+    Post.find(author_id).order(created_at: :desc).limit(3)
+  end
+end
