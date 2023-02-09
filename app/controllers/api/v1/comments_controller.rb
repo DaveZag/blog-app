c132bfa24d525ce41d37284b52a0db29ff0d6968
vitalis <vitalismutwiri2gmail.com>
2023-02-09 18:18:32 +0300

    Added comment api endpoint

--- app/controllers/api/v1/comments_controller.rb
+++ app/controllers/api/v1/comments_controller.rb
@@ -5,7 +5,19 @@ module Api
                 @comments = Comment.where(post: params[:post_id])
                 render json: @comments
             end
+            def create
+                @comment = Comment.new(comment_params)
+                if @comment.save
+                    render json: @comment, status: :created
+                else
+                    render json: 'Comment could not be created. Please try later.', status: :unprocessable_entity
+                end
+            end
             
+            private
+            def comment_params
+                params.require(:comment).permit(:post_id, :author_id, :text)
+            end
         end
     end
 end
